f0d055914a189a62bff3213012c3137c6dbdcc57
RiyaAhurasense404 <riya.singh@ahurasense.com>
2026-03-05 10:47:13 +0530

    Main.jsx completed

---
+++ src/components/layout/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='Nav'>
+        <h1>Home</h1>
+      
+    </div>
+  )
+}
+
+export default Navbar
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { AuthProvider } from "./context/AuthContext.jsx";
+import { WishListProvider } from "./context/WishListContext.jsx";
 
-createRoot(document.getElementById('root')).render(
+const queryClient = new QueryClient();
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <WishListProvider>
+          <App />
+        </WishListProvider>
+      </AuthProvider>
+    </QueryClientProvider>
+  </StrictMode>
+);
