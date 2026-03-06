c9a0907cfa635ac3ca50c06d2738777feff6ab13
RiyaAhurasense404 <riya.singh@ahurasense.com>
2026-03-06 18:32:55 +0530

    URL Filter commit

---
+++ src/hooks/useURLFilters.js
@@ -0,0 +1,19 @@
+import { useSearchParams } from "react-router-dom"
+import { useDebounce } from "./useDebounce"
+
+export const useURLFilters = () => {
+  const [searchParams, setSearchParams] = useSearchParams()
+
+  const city = searchParams.get("city")
+  const price = searchParams.get("price")
+
+  const debouncedCity = useDebounce(city, 1000)
+  const debouncedPrice = useDebounce(price, 1000)
+
+
+  const setFilters = (filters) => {
+    setSearchParams(filters)
+  }
+
+  return { city, price, setFilters , debouncedCity, debouncedPrice} 
+}
