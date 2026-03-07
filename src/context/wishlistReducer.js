2a1659fba3594110c01b7c4249daa15be4d94e98
RiyaAhurasense404 <riya.singh@ahurasense.com>
2026-03-07 11:44:29 +0530

    WishListPage commit

--- src/context/wishlistReducer.js
+++ src/context/wishlistReducer.js
@@ -4,7 +4,8 @@ const wishlistReducer = (state, action) => {
             return [...state, action.payload]
         case "Remove":
             return state.filter(item => item.id !== action.payload.id)
-
+        case "Clear":
+            return []
             default:
                 return state
 
--- src/pages/WishListPage.jsx
+++ src/pages/WishListPage.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import { useWishList } from "../hooks/useWishList"
+import HotelCard from "../components/hotel/HotelCard"
 
 const WishListPage = () => {
+  const { wishlist, dispatch } = useWishList()
+
+  if(wishlist.length === 0) return <p>Wishlist Empty!</p>
+
   return (
     <div>
-      
+      <button onClick={() => dispatch({ type: "Clear" })}>
+        Clear full wishlist 🗑️
+      </button>
+      {wishlist.map(hotel => (
+        <div key={hotel.id}>
+          <HotelCard hotel={hotel} />
+          <button onClick={() => dispatch({ type: "Remove", payload: hotel })}>
+            Remove ❌
+          </button>
+        </div>
+      ))}
     </div>
   )
 }
-
 export default WishListPage
